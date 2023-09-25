87888de7929f86ed2381258686b98eb19ef07e23
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-09-25 19:07:40 +0000

    all done

--- DOM/domi.js
+++ DOM/domi.js
@@ -2,9 +2,19 @@ var a = document.getElementsByClassName( 'un3');
 
 console.log(a);
 
-const b = document.querySelector('li');
+var b = document.querySelector('li');
+
+/*query selector bass ek e selet karega  */
+/* HTML collection is done with common class name  which is not an array since it has absence of the features of array */
+/* to convert the html collection to array we can do : Array.from(b);   */
 
 console.log(b);
+
+var c = document.querySelectorAll('#mama');
+
+/* selector all gives node list which has for each for traversal */
+
+console.log(c);
 
 
 
